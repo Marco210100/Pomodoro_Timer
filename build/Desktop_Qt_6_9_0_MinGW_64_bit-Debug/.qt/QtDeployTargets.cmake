a5b600059c55c9e65250cfb8eeb728560c1afebb
Marco210100 <Marco.Termini@Outlook.de>
2025-04-22 13:12:26 +0200

    Delete build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Pomodoro_Timer_FILE C:/Users/Marco/Documents/Pomodoro_Timer/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Pomodoro_Timer.exe)
-set(__QT_DEPLOY_TARGET_Pomodoro_Timer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Pomodoro_Timer_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
